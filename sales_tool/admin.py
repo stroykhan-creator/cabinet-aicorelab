6a878666be7bf849e5c7840e30de7f1d51317e7b
/usr/bin/vim <support@beget.ru>
2026-04-09 23:12:48 +0300

    Fix admin.py for sales_tool (clean)

--- sales_tool/admin.py
+++ sales_tool/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Product, ClientProfile, Treatment, CalculationTemplate, Proposal, ButtonAction
+from .models import (
+    Product,
+    ClientProfile,
+    Treatment,
+    CalculationTemplate,
+    Proposal,
+    ButtonAction,
+)
+
 
 class TreatmentInline(admin.TabularInline):
     model = Treatment
     extra = 0
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -11,26 +20,31 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('sku', 'name')
     readonly_fields = ('created_at', 'updated_at')
 
+
 @admin.register(ClientProfile)
 class ClientProfileAdmin(admin.ModelAdmin):
     list_display = ('patient', 'manager', 'phone', 'last_visit')
     search_fields = ('patient__name', 'manager', 'phone')
-    inlines = [TreatmentInline]
+    # inline removed (Treatment does not FK to ClientProfile)
+
 
 @admin.register(Treatment)
 class TreatmentAdmin(admin.ModelAdmin):
     list_display = ('patient', 'procedure', 'date', 'doctor')
     search_fields = ('patient__name', 'procedure', 'doctor')
 
+
 @admin.register(CalculationTemplate)
 class CalculationTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'company')
     search_fields = ('name', 'description')
 
+
 @admin.register(Proposal)
 class ProposalAdmin(admin.ModelAdmin):
     list_display = ('id', 'client', 'template', 'created_at')
     search_fields = ('client__name',)
+
 
 @admin.register(ButtonAction)
 class ButtonActionAdmin(admin.ModelAdmin):
